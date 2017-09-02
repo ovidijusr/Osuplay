cb0dfa1c8ca8f67717b8f09faa14101de2dad168
Ovidijus R <ovidijus@adapt.dk>
2017-09-02 11:21:42 +0300

    Add initial structure Move legacy app to legacy folder

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import FaBeer from 'react-icons/lib/fa/beer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        Welcome to Osuplay
+      </div>
+    );
+  }
+}
+
+export default App;
