d491767d4aaac15708e3745801ba4929cc9aa394
Ovidijus R <info@ovi.lt>
2018-04-22 12:52:17 +0300

    v2 - add customer seeker, todo: fix setting time

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import { VideoSeekSlider } from 'react-video-seek-slider';
 import './Footer.css'
+import '../components/VideoSeekSlider.css';
 import Player from '../components/Player';
-import PlayerSeeker from '../components/PlayerSeeker';
+// import PlayerSeeker from '../components/PlayerSeeker';
 import PlayerButton from '../components/PlayerButton';
 import * as actions from '../actions/player';
 import playButton from '../images/icons/music-play.svg';
 import pauseButton from '../images/icons/music-pause.svg';
-class Footer extends Component {
 
+class Footer extends Component {
   render() {
     const { player, actions } = this.props;
     return (
         <div className="footer">
+          <VideoSeekSlider
+            max={this.props.player.totalTime}
+            currentTime={this.props.player.currentTime}
+            progress={400}
+            offset={0}
+            onChange={(newTime) => {
+              this.props.actions.setCurrentTime(newTime)
+            }}
+            secondsPrefix="00:00:"
+            minutesPrefix="00:"
+          />
           <PlayerButton
             className="player-button"
             onClick={() => this.props.actions.togglePause()}
@@ -22,10 +35,10 @@ class Footer extends Component {
             }
             src=""
           />
-          <PlayerSeeker
+          {/* <PlayerSeeker
             currentTime={player.currentTime}
             totalTime={player.totalTime}
-          />
+          /> */}
           <Player
             {...({...player, actions: {...actions}} )}
           />
